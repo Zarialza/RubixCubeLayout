c89c1c6025a3feda4cd45d84a72b38f472d2c283
Sajan Shrestha <sajanbscsit2077@prime.edu.np>
2024-02-04 01:47:36 +0545

    color data of each face collected

--- main.js
+++ main.js
@@ -22,3 +22,57 @@ resetButton.addEventListener("click", () => {
     face.style.backgroundColor = "white";
   });
 });
+
+const face1Color = [];
+const face2Color = [];
+const face3Color = [];
+const face4Color = [];
+const face5Color = [];
+const face6Color = [];
+
+function collectColors() {
+  face1Color.length = 0; // Clear the array before collecting new colors
+  face2Color.length = 0;
+  face3Color.length = 0;
+  face4Color.length = 0;
+  face5Color.length = 0;
+  face6Color.length = 0;
+
+  const face1Cubes = document.querySelectorAll("#face1 .cubes");
+  const face2Cubes = document.querySelectorAll("#face2 .cubes");
+  const face3Cubes = document.querySelectorAll("#face3 .cubes");
+  const face4Cubes = document.querySelectorAll("#face4 .cubes");
+  const face5Cubes = document.querySelectorAll("#face5 .cubes");
+  const face6Cubes = document.querySelectorAll("#face6 .cubes");
+
+  face1Cubes.forEach((cube) => {
+    face1Color.push(cube.style.backgroundColor);
+  });
+
+  face2Cubes.forEach((cube) => {
+    face2Color.push(cube.style.backgroundColor);
+  });
+
+  face3Cubes.forEach((cube) => {
+    face3Color.push(cube.style.backgroundColor);
+  });
+
+  face4Cubes.forEach((cube) => {
+    face4Color.push(cube.style.backgroundColor);
+  });
+
+  face5Cubes.forEach((cube) => {
+    face5Color.push(cube.style.backgroundColor);
+  });
+
+  face6Cubes.forEach((cube) => {
+    face6Color.push(cube.style.backgroundColor);
+  });
+
+  console.log("Face 1 Colors:", face1Color);
+  console.log("Face 2 Colors:", face2Color);
+  console.log("Face 3 Colors:", face3Color);
+  console.log("Face 4 Colors:", face4Color);
+  console.log("Face 5 Colors:", face5Color);
+  console.log("Face 6 Colors:", face6Color);
+}
