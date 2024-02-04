0722c5b89a6e4b93f344e1a3544fb3b0ffdb0f27
Sajan Shrestha <sajanbscsit2077@prime.edu.np>
2024-02-04 21:30:10 +0545

    refactored main.js

--- main.js
+++ main.js
@@ -4,178 +4,67 @@ function setSelectedFace(faceColor) {
   selectedFace = faceColor;
 }
 
+function applyColor(cube, color) {
+  cube.style.backgroundColor = color;
+}
+
+function getRandomColor(colors, colorCount) {
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  if (colorCount[randomColor] === undefined || colorCount[randomColor] < 9) {
+    colorCount[randomColor] = (colorCount[randomColor] || 0) + 1;
+    return randomColor;
+  } else {
+    const unusedColors = colors.filter((color) => colorCount[color] < 9);
+    const newRandomColor =
+      unusedColors[Math.floor(Math.random() * unusedColors.length)];
+    colorCount[newRandomColor] = (colorCount[newRandomColor] || 0) + 1;
+    return newRandomColor;
+  }
+}
+
+function resetCubeLayout() {
+  const cubeFaces = document.querySelectorAll(".cubes");
+  cubeFaces.forEach((face) => {
+    face.style.backgroundColor = "white";
+  });
+}
+
+function collectColors() {
+  const faceColors = [];
+  for (let i = 1; i <= 6; i++) {
+    const faceCubes = document.querySelectorAll(`#face${i} .cubes`);
+    const faceColor = Array.from(
+      faceCubes,
+      (cube) => cube.style.backgroundColor
+    );
+    faceColors.push(faceColor);
+  }
+  console.log("Face Colors:", faceColors);
+}
+
+function randomizeCubeLayout() {
+  const colors = ["red", "blue", "green", "yellow", "orange", "grey"];
+  const colorCount = {};
+
+  for (let i = 1; i <= 6; i++) {
+    const faceCubes = document.querySelectorAll(`#face${i} .cubes`);
+    faceCubes.forEach((cube) => {
+      const randomColor = getRandomColor(colors, colorCount);
+      applyColor(cube, randomColor);
+    });
+  }
+}
+
+// Event listeners
 const cubeFaces = document.querySelectorAll(".cubes");
 
 cubeFaces.forEach((face) => {
   face.addEventListener("click", () => {
     if (selectedFace) {
-      face.style.backgroundColor = selectedFace;
+      applyColor(face, selectedFace);
     }
   });
 });
-
-//now create a reset button to reset the cube layout
 
 const resetButton = document.querySelector(".reset");
-resetButton.addEventListener("click", () => {
-  cubeFaces.forEach((face) => {
-    face.style.backgroundColor = "white";
-  });
-});
-
-const face1Color = [];
-const face2Color = [];
-const face3Color = [];
-const face4Color = [];
-const face5Color = [];
-const face6Color = [];
-
-function collectColors() {
-  face1Color.length = 0; // Clear the array before collecting new colors
-  face2Color.length = 0;
-  face3Color.length = 0;
-  face4Color.length = 0;
-  face5Color.length = 0;
-  face6Color.length = 0;
-
-  const face1Cubes = document.querySelectorAll("#face1 .cubes");
-  const face2Cubes = document.querySelectorAll("#face2 .cubes");
-  const face3Cubes = document.querySelectorAll("#face3 .cubes");
-  const face4Cubes = document.querySelectorAll("#face4 .cubes");
-  const face5Cubes = document.querySelectorAll("#face5 .cubes");
-  const face6Cubes = document.querySelectorAll("#face6 .cubes");
-
-  face1Cubes.forEach((cube) => {
-    face1Color.push(cube.style.backgroundColor);
-  });
-
-  face2Cubes.forEach((cube) => {
-    face2Color.push(cube.style.backgroundColor);
-  });
-
-  face3Cubes.forEach((cube) => {
-    face3Color.push(cube.style.backgroundColor);
-  });
-
-  face4Cubes.forEach((cube) => {
-    face4Color.push(cube.style.backgroundColor);
-  });
-
-  face5Cubes.forEach((cube) => {
-    face5Color.push(cube.style.backgroundColor);
-  });
-
-  face6Cubes.forEach((cube) => {
-    face6Color.push(cube.style.backgroundColor);
-  });
-
-  console.log("Face 1 Colors:", face1Color);
-  console.log("Face 2 Colors:", face2Color);
-  console.log("Face 3 Colors:", face3Color);
-  console.log("Face 4 Colors:", face4Color);
-  console.log("Face 5 Colors:", face5Color);
-  console.log("Face 6 Colors:", face6Color);
-}
-
-function randomize() {
-  const colors = ["red", "blue", "green", "yellow", "orange", "grey"];
-  const face1Cubes = document.querySelectorAll("#face1 .cubes");
-  const face2Cubes = document.querySelectorAll("#face2 .cubes");
-  const face3Cubes = document.querySelectorAll("#face3 .cubes");
-  const face4Cubes = document.querySelectorAll("#face4 .cubes");
-  const face5Cubes = document.querySelectorAll("#face5 .cubes");
-  const face6Cubes = document.querySelectorAll("#face6 .cubes");
-
-  const colorCount = {};
-
-  face1Cubes.forEach((cube) => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    cube.style.backgroundColor = randomColor;
-
-    if (colorCount[randomColor] === undefined || colorCount[randomColor] < 9) {
-      cube.style.backgroundColor = randomColor;
-      colorCount[randomColor] = (colorCount[randomColor] || 0) + 1;
-    } else {
-      const unusedColors = colors.filter((color) => colorCount[color] < 9);
-      const newRandomColor =
-        unusedColors[Math.floor(Math.random() * unusedColors.length)];
-      cube.style.backgroundColor = newRandomColor;
-      colorCount[newRandomColor] = (colorCount[newRandomColor] || 0) + 1;
-    }
-  });
-
-  face2Cubes.forEach((cube) => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    cube.style.backgroundColor = randomColor;
-    if (colorCount[randomColor] === undefined || colorCount[randomColor] < 9) {
-      cube.style.backgroundColor = randomColor;
-      colorCount[randomColor] = (colorCount[randomColor] || 0) + 1;
-    } else {
-      const unusedColors = colors.filter((color) => colorCount[color] < 9);
-      const newRandomColor =
-        unusedColors[Math.floor(Math.random() * unusedColors.length)];
-      cube.style.backgroundColor = newRandomColor;
-      colorCount[newRandomColor] = (colorCount[newRandomColor] || 0) + 1;
-    }
-  });
-
-  face3Cubes.forEach((cube) => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    cube.style.backgroundColor = randomColor;
-    if (colorCount[randomColor] === undefined || colorCount[randomColor] < 9) {
-      cube.style.backgroundColor = randomColor;
-      colorCount[randomColor] = (colorCount[randomColor] || 0) + 1;
-    } else {
-      const unusedColors = colors.filter((color) => colorCount[color] < 9);
-      const newRandomColor =
-        unusedColors[Math.floor(Math.random() * unusedColors.length)];
-      cube.style.backgroundColor = newRandomColor;
-      colorCount[newRandomColor] = (colorCount[newRandomColor] || 0) + 1;
-    }
-  });
-
-  face4Cubes.forEach((cube) => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    cube.style.backgroundColor = randomColor;
-    if (colorCount[randomColor] === undefined || colorCount[randomColor] < 9) {
-      cube.style.backgroundColor = randomColor;
-      colorCount[randomColor] = (colorCount[randomColor] || 0) + 1;
-    } else {
-      const unusedColors = colors.filter((color) => colorCount[color] < 9);
-      const newRandomColor =
-        unusedColors[Math.floor(Math.random() * unusedColors.length)];
-      cube.style.backgroundColor = newRandomColor;
-      colorCount[newRandomColor] = (colorCount[newRandomColor] || 0) + 1;
-    }
-  });
-
-  face5Cubes.forEach((cube) => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    cube.style.backgroundColor = randomColor;
-    if (colorCount[randomColor] === undefined || colorCount[randomColor] < 9) {
-      cube.style.backgroundColor = randomColor;
-      colorCount[randomColor] = (colorCount[randomColor] || 0) + 1;
-    } else {
-      const unusedColors = colors.filter((color) => colorCount[color] < 9);
-      const newRandomColor =
-        unusedColors[Math.floor(Math.random() * unusedColors.length)];
-      cube.style.backgroundColor = newRandomColor;
-      colorCount[newRandomColor] = (colorCount[newRandomColor] || 0) + 1;
-    }
-  });
-
-  face6Cubes.forEach((cube) => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    cube.style.backgroundColor = randomColor;
-    if (colorCount[randomColor] === undefined || colorCount[randomColor] < 9) {
-      cube.style.backgroundColor = randomColor;
-      colorCount[randomColor] = (colorCount[randomColor] || 0) + 1;
-    } else {
-      const unusedColors = colors.filter((color) => colorCount[color] < 9);
-      const newRandomColor =
-        unusedColors[Math.floor(Math.random() * unusedColors.length)];
-      cube.style.backgroundColor = newRandomColor;
-      colorCount[newRandomColor] = (colorCount[newRandomColor] || 0) + 1;
-    }
-  });
-}
+resetButton.addEventListener("click", resetCubeLayout);
